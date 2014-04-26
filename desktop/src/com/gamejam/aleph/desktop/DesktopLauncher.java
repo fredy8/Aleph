9d0fb0648c6d342ccadc4e7ed08a8e3ea9d77a9a
Alfredo Altamirano <fredy.altamirano8@gmail.com>
2014-04-25 22:55:08 -0500

    Project Setup

---
+++ desktop/src/com/gamejam/aleph/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gamejam.aleph.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gamejam.aleph.Aleph;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Aleph(), config);
+	}
+}
---
+++ html/src/com/gamejam/aleph/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.gamejam.aleph.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.gamejam.aleph.Aleph;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Aleph();
+        }
+}
